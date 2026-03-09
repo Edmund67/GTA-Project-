c3023dfefac24ddf3dd9965fb4131535cb72f981
Edmund67 <ekyeremeh@ftstudent.org>
2026-03-09 10:50:07 -0400

    hi
    hi

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+const SPEED = 60
+
+var direction = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.x +=1
+func _process(delta):
+	position.x += direction * SPEED * delta
