f53b243af6408d8da0e187d202520538045761c3
Edmund67 <ekyeremeh@ftstudent.org>
2026-03-10 10:47:53 -0400

    Hi
    Hi

---
+++ killzone.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _on_body_entered(body):
+	print("You Died Cuh!")
+	Engine.time_scale = 0.5
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
+	
+	
