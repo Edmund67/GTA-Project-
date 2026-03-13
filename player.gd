3cc30961ee916e3341c90c80572b951f21557c60
Edmund67 <ekyeremeh@ftstudent.org>
2026-03-13 10:01:57 -0400

    Hi
    Hi

--- player.gd
+++ player.gd
@@ -28,6 +28,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true 
 		
 		# Play animations
+	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
